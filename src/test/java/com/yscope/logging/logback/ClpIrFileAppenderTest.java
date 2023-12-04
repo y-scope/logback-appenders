f873d3db1805b0878ccc7bd622291faa159ab73b
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-12-04 07:49:42 -0500

    Bug-fix: Run four-byte or eight-byte encoding tests, not both.

--- src/test/java/com/yscope/logging/logback/ClpIrFileAppenderTest.java
+++ src/test/java/com/yscope/logging/logback/ClpIrFileAppenderTest.java
@@ -72,12 +72,9 @@ public class ClpIrFileAppenderTest {
 
     // Test writing
     try {
-      testWriting(fileName, false, false, compressionLevel);
-      testWriting(fileName, false, true, compressionLevel);
-      testWriting(fileName, false, false, compressionLevel + 1);
-      testWriting(fileName, true, false, compressionLevel);
-      testWriting(fileName, true, true, compressionLevel);
-      testWriting(fileName, true, false, compressionLevel + 1);
+      testWriting(fileName, useFourByteEncoding, false, compressionLevel);
+      testWriting(fileName, useFourByteEncoding, true, compressionLevel);
+      testWriting(fileName, useFourByteEncoding, false, compressionLevel + 1);
     } catch (Exception ex) {
       fail(ex);
     }
